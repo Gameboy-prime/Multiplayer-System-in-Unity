0d0a1ba5848cfdfd6051d682da2c9c3329d1e523
Craigshakesphere-Makinde <117420707+Craigshakesphere-Makinde@users.noreply.github.com>
2023-08-29 15:35:02 +0100

    Create PlayerListMenu.cs

---
+++ Multiplayer system/Assets/script/UI/PlayerListMenu.cs
@@ -0,0 +1,12 @@
+using Photon.Pun;
+using Photon.Realtime;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerListMenu : MonoBehaviourPunCallbacks
+{
+    public override void OnPlay
+    
+}
